3c5306fd59d4dcf219d29d4835d2b74248b2b734
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2024-12-11 14:57:10 +0800

    Update gost.sh

--- sh/gost.sh
+++ sh/gost.sh
@@ -100,13 +100,13 @@ function Install_ct()
     check_sys
     check_new_ver
     `rm -rf gost-linux-"$bit"-"$ct_new_ver".gz`
-    `wget --no-check-certificate https://github.com/dogwalkerg/gost/releases/download/v"$ct_new_ver"/gost-linux-"$bit"-"$ct_new_ver".gz`
+    `wget --no-check-certificate https://github.com/KANIKIG/gost/releases/download/v"$ct_new_ver"/gost-linux-"$bit"-"$ct_new_ver".gz`
     `gunzip gost-linux-"$bit"-"$ct_new_ver".gz`
     `mv gost-linux-"$bit"-"$ct_new_ver" gost`
     `mv gost /usr/bin/gost`
     `chmod -R 777 /usr/bin/gost`
-    `wget --no-check-certificate https://raw.githubusercontent.com/dogwalkerg/Multi-EasyGost/master/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system`
-    `mkdir /etc/gost && wget --no-check-certificate https://raw.githubusercontent.com/dogwalkerg/Multi-EasyGost/master/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost`
+    `wget --no-check-certificate https://raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system`
+    `mkdir /etc/gost && wget --no-check-certificate https://raw.githubusercontent.com/KANIKIG/Multi-EasyGost/master/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost`
     `systemctl enable gost && systemctl restart gost`
     echo "------------------------------"
     if test -a /usr/bin/gost -a /usr/lib/systemctl/gost.service -a /etc/gost/config.json;then
